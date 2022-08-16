aad92a8137e1aa32aa2d1ba67bf995c304218619
unknown <iamshahirohit@gmail.com>
2022-08-16 21:14:05 +0545

    URL updated

--- URL_Test/Basic_URL/Basic_URL/urls.py
+++ URL_Test/Basic_URL/Basic_URL/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('urlApp.urls'))
+    path('', include('urlApp.urls'), name='urlApp')
 ]
