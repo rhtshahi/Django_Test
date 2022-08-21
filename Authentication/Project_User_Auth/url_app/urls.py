e28d7efaa466d9445c954a770e339852e0f0f4e5
unknown <iamshahirohit@gmail.com>
2022-08-21 15:29:20 +0545

    Authentication

---
+++ Authentication/Project_User_Auth/url_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+]
